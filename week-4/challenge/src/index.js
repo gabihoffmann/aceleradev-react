ba5e7a37f6cb3816e864815bb9489874c00aea98
gabihoffmann <ah.gabriella@gmail.com>
2020-05-04 22:12:20 -0300

    Start week 4 challenge

---
+++ week-4/challenge/src/index.js
@@ -0,0 +1,9 @@
+const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
+
+function getShoppingCart(ids, productsList) {
+	
+	
+	return {};
+}
+
+module.exports = { getShoppingCart };
