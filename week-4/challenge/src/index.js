a0fa6dad3c569346c151833d28b0576f2bcfedbe
gabihoffmann <ah.gabriella@gmail.com>
2020-05-04 22:21:55 -0300

    Simulating shopping cart challenge

--- week-4/challenge/src/index.js
+++ week-4/challenge/src/index.js
@@ -1,9 +1,14 @@
 const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
+const produto = require('../src/produto');
 
 function getShoppingCart(ids, productsList) {
+	let {products} = produto.filterNamesCategory(ids,productsList);
+	let {promotion} = produto.filterPromotion(ids,productsList);
+	let {totalPrice} = produto.discountedAmount(ids,productsList);
+	let {discountValue} = produto.discountValue(ids,productsList);
+	let {discount} = produto.discountPercentage(ids,productsList);
 	
-	
-	return {};
+	return {products, promotion,totalPrice,discountValue,discount};
 }
 
 module.exports = { getShoppingCart };
