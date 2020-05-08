b438d59deccdd91e153243a31b55e4c75717b08b
gabihoffmann <ah.gabriella@gmail.com>
2020-05-08 10:04:02 -0300

    using react components and props

--- week-5/class-one/src/index.js
+++ week-5/class-one/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 function tick(){
@@ -15,7 +15,27 @@ function tick(){
 
 }
 
+//Criando um componente com function
+function Welcome(props){
+return <h1>Hello, {props.name}</h1>
+}
 
+//Criando um componente com Class
+class WelcomeOne extends Component{
+  render(){
+    return(
+    <h1>Hello, {this.props.name}</h1>
+    )
+  }
+}
+/**
+ * Os dois componentes acima são equivalentes do ponto de vista do React.
+ */
 
+const element = <Welcome name="Gabi"/>
+ReactDOM.render(
+  <Welcome name="name"/>,
+  document.getElementById('root')
+);
 
-setInterval(tick, 2000)
+//setInterval(tick, 2000)
